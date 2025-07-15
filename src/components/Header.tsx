ed6dbd857a794276a2fd732a20c0baca9742d907
Breno <brenojbarbosas@gmail.com>
2025-07-15 11:20:27 -0300

    alteracoes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,27 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full py-4 px-6 flex justify-between items-center bg-black shadow">
-      <h1 className="text-xl font-bold text-white">FitTrack</h1>
+      <Link href="/">
+        <h1 className="text-xl font-bold text-white cursor-pointer">FitTrack</h1>
+      </Link>
+
       <nav>
         <ul className="flex gap-4 text-sm text-gray-300">
+      
           <li>
-            <a href="#features" className="hover:text-white transition">
+            <Link
+              href={{ pathname: "/", hash: "features" }} 
+              className="hover:text-white transition"
+            >
               Funcionalidades
-            </a>
+            </Link>
           </li>
+
+       
           <li>
-            <Link href="/newsletter">NewsLetter</Link>
+            <Link href="/newsletter" className="hover:text-white transition">
+              NewsLetter
+            </Link>
           </li>
         </ul>
       </nav>
